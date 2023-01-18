09c2d502b17b55ea64306a8daedd35bf27f8a5c7
mr-marco <mr.marcopelle@gmail.com>
2023-01-18 15:06:58 +0100

    schools with ESRI living atlas

--- location_folders/schools/schools.R
+++ location_folders/schools/schools.R
@@ -9,6 +9,8 @@ library(sf)
 ################################################################################
 # Load and filter DHZW
 
+# https://livingatlas-dcdev.opendata.arcgis.com/datasets/esrinl-content::onderwijslocaties-adres/explore?filters=eyJQUk9WSU5DSUUiOlsiWnVpZC1Ib2xsYW5kIl0sIkdFTUVFTlRFTkFBTSI6WyJTIEdSQVZFTkhBR0UiXX0%3D&location=52.053199%2C4.316303%2C14.58
+
 # Load PC4 DHZW
 setwd(this.path::this.dir())
 setwd('../../codes')
@@ -20,9 +22,21 @@ DHZW_PC4_codes <-
 # Load municipality schools
 setwd(this.path::this.dir())
 setwd('data/')
-df_schools <- st_read('schoolgebouwen')
+df_schools_esri <- st_read('DUO_Onderwijslocaties')
+df_schools_municipality <- st_read('schoolgebouwen')
 
-df_schools <- df_schools %>%
+df_schools_esri <- df_schools_esri %>%
+  select(ONDERWIJS,
+         INSTELLING,
+         POSTCODE) %>%
+  rename(level = ONDERWIJS,
+         name = INSTELLING,
+         PC6 = POSTCODE) %>%
+  mutate(PC6 = gsub(' ', '', PC6)) %>%
+  mutate(PC4 = gsub('.{2}$', '', PC6)) %>%
+  filter(PC4 %in% DHZW_PC4_codes)
+
+df_schools_municipality <- df_schools_municipality %>%
   select(bestemming,
          school,
          postcode) %>%
@@ -33,30 +47,41 @@ df_schools <- df_schools %>%
   mutate(PC4 = gsub('.{2}$', '', PC6)) %>%
   filter(PC4 %in% DHZW_PC4_codes)
 
-# Transform into WGS84
-df_schools <- st_transform(df_schools, "+proj=longlat +datum=WGS84")
-
 ################################################################################
-# retrieve coordinates from the geometry because the original ones are incomplete
-df_coordinates <- data.frame(st_coordinates(df_schools))
-colnames(df_coordinates) <- c('longitude', 'latitude')
-df_schools = cbind(df_schools, df_coordinates)
-df_schools <- data.frame(df_schools)
-df_schools = subset(df_schools, select = -c(geometry))
+# Filter and sssign categories
+
+# Use the municipality dataset for daycare
+df_schools_municipality <- df_schools_municipality[df_schools_municipality$level=='BO-Voorschool',]
+df_schools_municipality <- st_transform(df_schools_municipality, "+proj=longlat +datum=WGS84")
+
+# Use the ESRI living atlas for primary and highschool
+df_schools_esri <- df_schools_esri[df_schools_esri$level=='BO' | df_schools_esri$level=='VBO/MAVO',]
+df_schools_esri <- st_transform(df_schools_esri, "+proj=longlat +datum=WGS84")
+df_schools_esri <- st_zm (df_schools_esri)
+
+# Put dataset together and reformat school labels
+df_schools <- rbind(df_schools_municipality, df_schools_esri)
+df_schools$category <- NA
+df_schools[df_schools$level == 'BO-Voorschool',]$category <- 'daycare'
+df_schools[df_schools$level == 'BO',]$category <- 'primary_school'
+df_schools[df_schools$level == 'VBO/MAVO',]$category <- 'highschool'
+
+df_schools <- df_schools %>%
+  subset(select = -c(level))
 
 df_schools$type <- 'school'
 df_schools$lid <- paste0('school_', seq.int(nrow(df_schools)))
 
 ################################################################################
-# Manually assign the categories
+# retrieve coordinates from the geometry
 
-df_schools$category <- NA
-df_schools[df_schools$level == 'BO',]$category <- 'primary_school'
-df_schools[df_schools$level == 'VO' | df_schools$level == 'GYM',]$category <- 'highschool'
-df_schools[df_schools$level == 'BO-Voorschool',]$category <- 'daycare'
+df_coordinates <- data.frame(st_coordinates(df_schools))
+colnames(df_coordinates) <- c('longitude', 'latitude')
+df_schools = cbind(df_schools, df_coordinates)
+df_schools <- data.frame(df_schools)
 
-df_schools <- df_schools %>%
-  filter(!is.na(category)) %>%
-  subset(select = -c(type))
+plot(df_schools$geometry)
 
-write.csv(df_schools, 'schools_DHZW.csv', row.names = FALSE)
+################################################################################
+# Save
+st_write(df_schools, 'schools_DHZW/schools_DHZW.shp')
